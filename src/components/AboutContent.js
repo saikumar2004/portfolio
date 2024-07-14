608ab88d687d5a046c7e472b7888edbeea5609d3
SAI KUMAR@LAPTOP-52G4EK29 <saikumarkaicherla2004@gmail.com>
2024-07-15 00:03:19 +0530

    update

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -8,7 +8,7 @@ function AboutContent(){
     <div className="about">
        <div className="left">
         <h1>Who Am I?</h1>
-        <p>Im a MERN stack developer . i create responsive secure website</p>
+        <p>Im a MERN stack developer. I create responsive secure website</p>
         <Link to="/contact">
         <button className="btn">Contact</button>
         </Link>
