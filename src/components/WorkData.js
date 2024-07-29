bd9ca10883d6b3894d667bb4157a337a305e5d56
SAI KUMAR@LAPTOP-52G4EK29 <saikumarkaicherla2004@gmail.com>
2024-07-29 17:15:17 +0530

    update

--- src/components/WorkData.js
+++ src/components/WorkData.js
@@ -3,11 +3,20 @@ import pro2 from "../assets/project2.png";
 import pro3 from "../assets/project3.png";
 const WorkData=[
     {
+        imgsrc:pro3,
+        title:"university Website",
+        text:"Developed a comprehensive university website featuring a responsive design, dynamic content, and interactive components. Implemented key sections such as the Navbar, Hero section, Programs, About, Campus Gallery, Testimonials, Contact form, and integrated a VideoPlayer for multimedia content.",
+        tech:"React,CSS",
+        view:"https://saikumar2004.github.io/Sparksity-website/",
+        source:"https://github.com/saikumar2004/Sparksity-website"
+        
+    },
+    {
         imgsrc:pro1,
         title:"New-App ",
         text:"Developed a News Website that displays live news updates. Users can search for any article using the integrated search bar. The website leverages updated news articles from the NEWS-API, ensuring real-time and accurate information.",
         tech:"React,CSS,API",
-        view:"https://github.com/saikumar2004/News-app",
+        view:"https://saikumar2004.github.io/",
         source:"https://github.com/saikumar2004/News-app"
 
     },
@@ -17,17 +26,9 @@ const WorkData=[
         text:"The application features an intuitive interface that enables users to log their daily dietary intake, monitor calorie consumption, and track the quantity of food consumed. The app offers real-time feedback on nutrient levels, helping users make informed dietary choices and maintain a balanced diet.",
         tech:"MERN stack",
         view:"https://saikumar2004.github.io",
-        source:"https://saikumar2004.github.io"
-        
-    },
-    {
-        imgsrc:pro3,
-        title:"university Website",
-        text:"Developed a comprehensive university website featuring a responsive design, dynamic content, and interactive components. Implemented key sections such as the Navbar, Hero section, Programs, About, Campus Gallery, Testimonials, Contact form, and integrated a VideoPlayer for multimedia content.",
-        tech:"React,CSS",
-        view:"https://saikumar2004.github.io/Sparksity-website/",
-        source:"https://github.com/saikumar2004/Sparksity-website"
+        source:"https://github.com/saikumar2004/nutrition-tracker"
         
     }
+    
 ]
 export default WorkData;
