743357a8e662829a41ef743ecfef70fb58bc231c
SAI KUMAR@LAPTOP-52G4EK29 <saikumarkaicherla2004@gmail.com>
2024-08-22 18:02:04 +0530

    update

--- src/components/WorkData.js
+++ src/components/WorkData.js
@@ -1,7 +1,26 @@
 import pro1 from "../assets/project1.png";
 import pro2 from "../assets/project2.png";
 import pro3 from "../assets/project3.png";
+import pro4 from "../assets/project4.png"
 const WorkData=[
+    {
+        imgsrc:pro4,
+        title:"Spotify-clone",
+        text:"Developed a Spotify Clone using the MERN stack to provide a comprehensive music streaming experience.Includes music playback, and playlist management features. Integrated a robust backend with Express and Node.js Utilized MongoDB for data storage and React for a responsive, dynamic frontend interface,enhancing user experience with real-time updates and seamless navigation.",
+        tech:"MERN stack",
+        view:"https://saikumar2004.github.io/spotify-clone-MERNstack/",
+        source:"https://github.com/saikumar2004/spotify-clone-MERNstack"
+        
+    },  
+    {
+        imgsrc:pro2,
+        title:"Nutrient-Tracker-App",
+        text:"The application features an intuitive interface that enables users to log their daily dietary intake, monitor calorie consumption, and track the quantity of food consumed. The app offers real-time feedback on nutrient levels, helping users make informed dietary choices and maintain a balanced diet.",
+        tech:"MERN stack",
+        view:"https://saikumar2004.github.io/nutrition-tracker/",
+        source:"https://github.com/saikumar2004/nutrition-tracker"
+        
+    },
     {
         imgsrc:pro3,
         title:"university Website",
@@ -19,16 +38,8 @@ const WorkData=[
         view:"https://saikumar2004.github.io/",
         source:"https://github.com/saikumar2004/News-app"
 
-    },
-    {
-        imgsrc:pro2,
-        title:"Nutrient-Tracker-App",
-        text:"The application features an intuitive interface that enables users to log their daily dietary intake, monitor calorie consumption, and track the quantity of food consumed. The app offers real-time feedback on nutrient levels, helping users make informed dietary choices and maintain a balanced diet.",
-        tech:"MERN stack",
-        view:"https://saikumar2004.github.io",
-        source:"https://github.com/saikumar2004/nutrition-tracker"
-        
     }
+    
     
 ]
 export default WorkData;
